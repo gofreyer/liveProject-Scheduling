65f3c91582ec19fd249a02db2740cdd9e1c8c77e
Geerd-Olaf Freyer <83983741+gofreyer@users.noreply.github.com>
2022-09-22 17:18:21 +0200

    Add files via upload

---
+++ Scheduling2_PertCharts/PoSorter.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Windows;
+
+namespace pert_charts
+{
+    class PoSorter
+    {
+        public List<Task> Tasks { get; set; }
+        public List<Task> SortedTasks { get; set; }
+        public PoSorter()
+        {
+            Tasks = null;
+            SortedTasks = null;
+        }
+        public void TopoSort()
+        {
+            if (Tasks == null)
+            {
+                return;
+            }
+            SortedTasks = new List<Task>();
+            Queue<Task> readyTasks = new Queue<Task>();
+            
+            foreach (Task task in Tasks)
+            {
+                task.Init();
+            }
+            foreach (Task task in Tasks)
+            {
+                task.AddToFollowerLists();
+                if (task.PrereqCount == 0)
+                {
+                    readyTasks.Enqueue(task);
+                }
+            }
+
+            while (readyTasks.Count > 0)
+            {
+                Task readyTask = readyTasks.Dequeue();
+                SortedTasks.Add(readyTask);
+                foreach (Task task in readyTask.FollowerTasks)
+                {
+                    task.PrereqCount--;
+                    if (task.PrereqCount == 0)
+                    {
+                        readyTasks.Enqueue(task);
+                    }
+                }
+            }
+        }
+        
+        public void VerifySort()
+        {
+            if (SortedTasks == null)
+            {
+                return;
+            }
+            int countSorted = 0;
+            int countTasks = 0;
+            for (int i = 0; i < Tasks.Count; i++)
+            {
+                Tasks[i].SortIndex = -1;
+            }
+            for (int i = 0; i < SortedTasks.Count; i++)
+            {
+                SortedTasks[i].SortIndex = i;
+            }
+            foreach (Task task in Tasks)
+            {
+                countTasks++;
+                if (task.PrereqTasks.TrueForAll(t => t.SortIndex < task.SortIndex))
+                {
+                    countSorted++;
+                }
+            }
+            if (countSorted < countTasks)
+            {
+                MessageBox.Show($"Sorted only {countSorted} out of {countTasks} tasks.");
+            }
+            else
+            {
+                MessageBox.Show($"Successfully sorted {countSorted} out of {countTasks} tasks.");
+            }
+            
+        }
+        public Task ReadTask(StreamReader _sr)
+        {
+            while (true)
+            {
+                string line = _sr.ReadLine();
+                if (line == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    line = line.Trim();
+                    if (line.Length > 0)
+                    {
+                        var tokens = new List<String>();
+                        int start = 0;
+                        int count = 0;
+                        int position;
+                        do
+                        {
+                            position = line.IndexOf(',', start);
+                            if (position >= 0)
+                            {
+                                count++;
+                                tokens.Add(line.Substring(start, position - start).Trim());
+                                start = position + 1;
+                            }
+                        } while (position > 0 && count < 2);
+                        if (count == 2)
+                        {
+                            tokens.Add(line.Substring(start).Trim());
+
+                            List<int> pre = new List<int>();
+
+                            string[] prenumbers = tokens[2].Split(',');
+                            foreach (string num in prenumbers)
+                            {
+                                string num2 = num.Replace("[", "").Replace("]", "").Trim();
+                                if (num2 != "")
+                                {
+                                    pre.Add(int.Parse(num2));
+                                }
+                            }
+
+                            Task task = new Task(int.Parse(tokens[0]), tokens[1], pre);
+
+                            return task;
+                        }
+                                               
+                        break;
+                    }
+                }
+            }
+            return null;
+        }
+        public void LoadPoFile(string _filename)
+        {
+            Tasks = new List<Task>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(_filename))
+                {
+                    Task task;
+                    while ((task = ReadTask(sr)) != null)
+                    {
+                        Tasks.Add(task);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            foreach(Task task in Tasks)
+            {
+                task.NumbersToTasks(Tasks);
+            }
+        }
+    }
+}
---
+++ Scheduling2_PertCharts/Task.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pert_charts
+{
+    internal class Task
+    {
+        public string Name { get; set; }
+        public int Index { get; set; }
+        public int PrereqCount { get;set; }
+        public List<int> PrereqNumbers { get; set; }
+        public List<Task> PrereqTasks { get; set; }
+        public List<Task> FollowerTasks { get; set; }
+        public int SortIndex { get; set; }
+        public Task(int _index, string _name, List<int> _prereqNumbers)
+        {
+            Index = _index;
+            Name = _name;
+            PrereqNumbers = _prereqNumbers;
+        }
+
+        public override string ToString()
+        {
+            return Name.ToString();
+            
+            // for debugging
+            /*
+            string temp = "";
+            int numPrereqs = PrereqNumbers.Count;
+            for (int i = 0; i < numPrereqs; i++)
+            {
+                temp += " "+PrereqNumbers[i].ToString();
+            }
+            return $"{Index.ToString()} - {Name.ToString()} - [{temp}]";
+            */
+        }
+        public void Init()
+        {
+            PrereqCount = PrereqTasks.Count;
+            FollowerTasks = new List<Task>();
+        }
+        public void AddToFollowerLists()
+        { 
+            foreach (Task task in PrereqTasks)
+            {
+                task.FollowerTasks.Add(this);
+            }
+        }
+        public void NumbersToTasks(List<Task> tasks)
+        {
+            int numPrereqs = PrereqNumbers.Count;
+            PrereqTasks = new List<Task>();
+            for (int i = 0; i < numPrereqs; i++)
+            {
+                PrereqTasks.Add(tasks[PrereqNumbers[i]]);
+            }
+        }
+    }
+}
+
