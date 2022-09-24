5fb6d74f0816e6cdb85904afc048420682bcd7f1
Geerd-Olaf Freyer <83983741+gofreyer@users.noreply.github.com>
2022-09-24 20:43:45 +0200

    Add files via upload

---
+++ Scheduling3_CriticalPaths/PoSorter.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Documents;
+using System.Windows.Interop;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Media3D;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Windows.Controls;
+
+namespace critical_paths
+{
+    class PoSorter
+    {
+        public List<Task> Tasks { get; set; }
+        public List<Task> SortedTasks { get; set; }
+        public List<List<Task>> Columns { get; set; }
+        public PoSorter()
+        {
+            Tasks = null;
+            SortedTasks = null;
+            Columns = null;
+        }
+        public void PrepareTasks()
+        {
+            foreach (Task task in Tasks)
+            {
+                task.Init();
+            }
+            foreach (Task task in Tasks)
+            {
+                task.AddToFollowerLists();
+            }
+        }
+        public void TopoSort()
+        {
+            if (Tasks == null)
+            {
+                return;
+            }
+            SortedTasks = new List<Task>();
+            Queue<Task> readyTasks = new Queue<Task>();
+
+            PrepareTasks();
+            
+            foreach (Task task in Tasks)
+            {
+                if (task.PrereqCount == 0)
+                {
+                    readyTasks.Enqueue(task);
+                }
+            }
+
+            while (readyTasks.Count > 0)
+            {
+                Task readyTask = readyTasks.Dequeue();
+                SortedTasks.Add(readyTask);
+                foreach (Task task in readyTask.FollowerTasks)
+                {
+                    task.PrereqCount--;
+                    if (task.PrereqCount == 0)
+                    {
+                        readyTasks.Enqueue(task);
+                    }
+                }
+            }
+        }
+        public void BuildPertChart()
+        {
+            if (Tasks == null)
+            {
+                return;
+            }
+            SortedTasks = new List<Task>();
+            Queue<Task> readyTasks = new Queue<Task>();
+            Queue<Task> newReadyTasks = new Queue<Task>();
+
+            PrepareTasks();
+
+            foreach (Task task in Tasks)
+            {
+                if (task.PrereqCount == 0)
+                {
+                    readyTasks.Enqueue(task);
+                }
+            }
+
+            Columns = new List<List<Task>>();
+            List<Task> Column = new List<Task>();
+            Columns.Add(Column);
+            foreach (Task task in readyTasks)
+            {
+                Column.Add(task);
+            }
+
+            Task finishTask = null;
+            while (readyTasks.Count > 0)
+            {
+                Task readyTask = readyTasks.Dequeue();
+                finishTask = readyTask;
+                readyTask.SetTimes();
+                SortedTasks.Add(readyTask);
+                foreach (Task task in readyTask.FollowerTasks)
+                {
+                    task.PrereqCount--;
+                    if (task.PrereqCount == 0)
+                    {
+                        newReadyTasks.Enqueue(task);
+                    }
+                }
+                if (readyTasks.Count == 0)
+                {
+                    if (newReadyTasks.Count > 0)
+                    {
+                        Column = new List<Task>();
+                        Columns.Add(Column);
+                        foreach (Task task in newReadyTasks)
+                        {
+                            Column.Add(task);
+                        }
+                    }
+                    readyTasks = newReadyTasks;
+                    newReadyTasks = new Queue<Task>();
+                }
+            }
+            if (finishTask != null)
+            {
+                finishTask.MarkIsCritical();
+            }
+        }
+        public void DrawPertChart(Canvas _canvas)
+        {
+            _canvas.Children.Clear();
+
+            for (int col = 0; col < Columns.Count; col++)
+            {
+                for (int row = 0; row < Columns[col].Count; row++)
+                {
+                    Task task = Columns[col][row];
+                    task.SetBounds(col, row);
+                }
+            }
+
+            foreach (Task task in Tasks)
+            {
+                task.DrawLinesToPrereqs(_canvas);
+            }
+            foreach (Task task in Tasks)
+            {
+                task.DrawTaskBox(_canvas);
+            }
+        }
+        public void VerifySort()
+        {
+            if (SortedTasks == null)
+            {
+                return;
+            }
+            int countSorted = 0;
+            int countTasks = 0;
+            for (int i = 0; i < Tasks.Count; i++)
+            {
+                Tasks[i].SortIndex = -1;
+            }
+            for (int i = 0; i < SortedTasks.Count; i++)
+            {
+                SortedTasks[i].SortIndex = i;
+            }
+            foreach (Task task in Tasks)
+            {
+                countTasks++;
+                if (task.PrereqTasks.TrueForAll(t => t.SortIndex < task.SortIndex))
+                {
+                    countSorted++;
+                }
+            }
+            if (countSorted < countTasks)
+            {
+                MessageBox.Show($"Sorted only {countSorted} out of {countTasks} tasks.");
+            }
+            else
+            {
+                MessageBox.Show($"Successfully sorted {countSorted} out of {countTasks} tasks.");
+            }
+            
+        }
+        public Task ReadTask(StreamReader _sr)
+        {
+            while (true)
+            {
+                string line = _sr.ReadLine();
+                if (line == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    line = line.Trim();
+                    if (line.Length > 0)
+                    {
+                        var tokens = new List<String>();
+                        int start = 0;
+                        int count = 0;
+                        int position;
+                        do
+                        {
+                            position = line.IndexOf(',', start);
+                            if (position >= 0)
+                            {
+                                count++;
+                                tokens.Add(line.Substring(start, position - start).Trim());
+                                start = position + 1;
+                            }
+                        } while (position > 0 && count < 3);
+                        if (count == 3)
+                        {
+                            tokens.Add(line.Substring(start).Trim());
+
+                            List<int> pre = new List<int>();
+
+                            string[] prenumbers = tokens[3].Split(',');
+                            foreach (string num in prenumbers)
+                            {
+                                string num2 = num.Replace("[", "").Replace("]", "").Trim();
+                                if (num2 != "")
+                                {
+                                    pre.Add(int.Parse(num2));
+                                }
+                            }
+
+                            Task task = new Task(int.Parse(tokens[0]), tokens[2], int.Parse(tokens[1]), pre);
+
+                            return task;
+                        }
+                                               
+                        break;
+                    }
+                }
+            }
+            return null;
+        }
+        public void LoadPoFile(string _filename)
+        {
+            Tasks = new List<Task>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(_filename))
+                {
+                    Task task;
+                    while ((task = ReadTask(sr)) != null)
+                    {
+                        Tasks.Add(task);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            foreach(Task task in Tasks)
+            {
+                task.NumbersToTasks(Tasks);
+            }
+        }
+    }
+}
---
+++ Scheduling3_CriticalPaths/Task.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Xml.Linq;
+using System.Windows.Shapes;
+using critical_paths.Properties;
+using System.Collections;
+
+namespace critical_paths
+{
+    internal class Task
+    {
+        private const double MARGIN = 5;
+        private const double SIDE = 60;
+        private const double GAP = 40;
+        private const double FONT_SIZE = 11;
+        public Point Center { get; set; }
+        public Rect Bounds { get; set; }
+
+        public string Name { get; set; }
+        public int Index { get; set; }
+        public int Duration { get; set; }
+        public int PrereqCount { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public bool IsCritical { get; set; }
+        public List<int> PrereqNumbers { get; set; }
+        public List<Task> PrereqTasks { get; set; }
+        public List<Task> FollowerTasks { get; set; }
+        public int SortIndex { get; set; }
+        public Task(int _index, string _name, int _duration, List<int> _prereqNumbers)
+        {
+            Index = _index;
+            Name = _name;
+            Duration = _duration;
+            PrereqNumbers = _prereqNumbers;
+            Center = new Point(0, 0);
+            Bounds = new Rect(0, 0, 0, 0);
+            StartTime = 0;
+            EndTime = 0;
+            IsCritical = false;
+        }
+
+        public override string ToString()
+        {
+            //return Name.ToString();
+
+            // for debugging
+
+            string temp = "";
+            int numPrereqs = PrereqNumbers.Count;
+            for (int i = 0; i < numPrereqs; i++)
+            {
+                temp += " " + PrereqNumbers[i].ToString();
+            }
+            return $"{Index.ToString()} - {Name.ToString()} - ({Duration.ToString()}) - [{temp}]";
+
+        }
+        public void Init()
+        {
+            PrereqCount = PrereqTasks.Count;
+            FollowerTasks = new List<Task>();
+            IsCritical = false; 
+        }
+        public void AddToFollowerLists()
+        {
+            foreach (Task task in PrereqTasks)
+            {
+                task.FollowerTasks.Add(this);
+            }
+        }
+        public void NumbersToTasks(List<Task> tasks)
+        {
+            int numPrereqs = PrereqNumbers.Count;
+            PrereqTasks = new List<Task>();
+            for (int i = 0; i < numPrereqs; i++)
+            {
+                PrereqTasks.Add(tasks[PrereqNumbers[i]]);
+            }
+        }
+        public void SetBounds(int _col, int _row)
+        {
+            Center = new Point(MARGIN + _col * (SIDE + GAP) + SIDE / 2,
+                MARGIN + _row * (SIDE + MARGIN) + SIDE / 2);
+            Bounds = new Rect(Center.X - SIDE / 2, Center.Y - SIDE / 2, SIDE, SIDE);
+        }
+        public void DrawLinesToPrereqs(Canvas _canvas)
+        {
+            int lineWidth = 1;
+            Brush lineBrush = Brushes.Black;
+            Point ptTo = new Point(Center.X-SIDE/2,Center.Y);
+            foreach (Task task in PrereqTasks)
+            {
+                Point ptFrom = new Point(task.Center.X + SIDE / 2, task.Center.Y);
+                lineWidth = 1;
+                lineBrush = Brushes.Black;
+                
+                if (task.EndTime == StartTime)
+                {
+                    lineWidth = 3;
+                }
+                
+                if (IsCritical && task.EndTime == StartTime)
+                {
+                    lineBrush = Brushes.Red;
+                }
+                
+                Line line = _canvas.DrawLine(ptFrom, ptTo, lineBrush, lineWidth);
+            }
+        }
+        public void DrawTaskBox(Canvas _canvas)
+        {
+            Brush boxFill = Brushes.LightBlue;
+            Brush boxStroke = Brushes.Black;
+            Brush labelColor = Brushes.Black;
+            if (IsCritical)
+            {
+                boxFill = Brushes.Pink;
+                boxStroke = Brushes.Red;
+                labelColor = Brushes.Red;
+            }
+            _canvas.DrawRectangle(Bounds,boxFill,boxStroke, 2);
+            string label = $"Task: {Index.ToString()}\nDur: {Duration.ToString()}\nStart: {StartTime.ToString()}\nEnd: {EndTime.ToString()}";
+            _canvas.DrawLabel(Bounds,label, Brushes.Transparent, labelColor, (HorizontalAlignment)1, (VerticalAlignment)1, FONT_SIZE, 0);
+        }
+        public void SetTimes()
+        {
+            int maxEndTime = 0;
+            foreach (Task task in PrereqTasks)
+            {
+                if (task.EndTime > maxEndTime)
+                {
+                    maxEndTime = task.EndTime;
+                }
+            }
+            StartTime = maxEndTime;
+            EndTime = StartTime + Duration;
+        }
+        public void MarkIsCritical()
+        {
+            IsCritical = true;
+            foreach (Task task in PrereqTasks)
+            {
+                if (task.EndTime == StartTime)
+                {
+                    task.MarkIsCritical();
+                }
+            }
+        }
+    }
+}
+
+
+/*When drawing tasks:
+i. If the task is project critical, fill its rectangle with pink and draw its outline and text in red.
+ii. Else if the task is not project critical, fill its rectangle with light blue and draw its outline and text in black.
+iii. Draw the task’s index, duration, start time, and end time in its rectangle like this:
+Task 6
+Dur: 8
+Start: 25
+End: 33*/
