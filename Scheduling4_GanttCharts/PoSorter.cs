c9c873f4d1215d6b80672565129eaf4c1862ff06
Geerd-Olaf Freyer <83983741+gofreyer@users.noreply.github.com>
2022-09-25 15:26:39 +0200

    Add files via upload

---
+++ Scheduling4_GanttCharts/PoSorter.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Documents;
+using System.Windows.Interop;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Media3D;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace gantt_charts
+{
+    class PoSorter
+    {
+        public List<Task> Tasks { get; set; }
+        public List<Task> SortedTasks { get; set; }
+        public List<List<Task>> Columns { get; set; }
+        public PoSorter()
+        {
+            Tasks = null;
+            SortedTasks = null;
+            Columns = null;
+        }
+        public void PrepareTasks()
+        {
+            foreach (Task task in Tasks)
+            {
+                task.Init();
+            }
+            foreach (Task task in Tasks)
+            {
+                task.AddToFollowerLists();
+            }
+        }
+        public void TopoSort()
+        {
+            if (Tasks == null)
+            {
+                return;
+            }
+            SortedTasks = new List<Task>();
+            Queue<Task> readyTasks = new Queue<Task>();
+
+            PrepareTasks();
+            
+            foreach (Task task in Tasks)
+            {
+                if (task.PrereqCount == 0)
+                {
+                    readyTasks.Enqueue(task);
+                }
+            }
+
+            while (readyTasks.Count > 0)
+            {
+                Task readyTask = readyTasks.Dequeue();
+                SortedTasks.Add(readyTask);
+                foreach (Task task in readyTask.FollowerTasks)
+                {
+                    task.PrereqCount--;
+                    if (task.PrereqCount == 0)
+                    {
+                        readyTasks.Enqueue(task);
+                    }
+                }
+            }
+        }
+        public void BuildPertChart()
+        {
+            if (Tasks == null)
+            {
+                return;
+            }
+            SortedTasks = new List<Task>();
+            Queue<Task> readyTasks = new Queue<Task>();
+            Queue<Task> newReadyTasks = new Queue<Task>();
+
+            PrepareTasks();
+
+            foreach (Task task in Tasks)
+            {
+                if (task.PrereqCount == 0)
+                {
+                    readyTasks.Enqueue(task);
+                }
+            }
+
+            Columns = new List<List<Task>>();
+            List<Task> Column = new List<Task>();
+            Columns.Add(Column);
+            foreach (Task task in readyTasks)
+            {
+                Column.Add(task);
+            }
+
+            Task finishTask = null;
+            while (readyTasks.Count > 0)
+            {
+                Task readyTask = readyTasks.Dequeue();
+                finishTask = readyTask;
+                readyTask.SetTimes();
+                SortedTasks.Add(readyTask);
+                foreach (Task task in readyTask.FollowerTasks)
+                {
+                    task.PrereqCount--;
+                    if (task.PrereqCount == 0)
+                    {
+                        newReadyTasks.Enqueue(task);
+                    }
+                }
+                if (readyTasks.Count == 0)
+                {
+                    if (newReadyTasks.Count > 0)
+                    {
+                        Column = new List<Task>();
+                        Columns.Add(Column);
+                        foreach (Task task in newReadyTasks)
+                        {
+                            Column.Add(task);
+                        }
+                    }
+                    readyTasks = newReadyTasks;
+                    newReadyTasks = new Queue<Task>();
+                }
+            }
+            if (finishTask != null)
+            {
+                finishTask.MarkIsCritical();
+            }
+        }
+        public void DrawPertChart(Canvas _canvas)
+        {
+            _canvas.Children.Clear();
+
+            for (int col = 0; col < Columns.Count; col++)
+            {
+                for (int row = 0; row < Columns[col].Count; row++)
+                {
+                    Task task = Columns[col][row];
+                    task.SetBounds(col, row);
+                }
+            }
+
+            foreach (Task task in Tasks)
+            {
+                task.DrawLinesToPrereqs(_canvas);
+            }
+            double maxX = 0;
+            double maxY = 0;
+            foreach (Task task in Tasks)
+            {
+                task.DrawTaskBox(_canvas);
+                if (task.Bounds.Left+task.Bounds.Width+task.GetMargin() > maxX) maxX = task.Bounds.Left+task.Bounds.Width+task.GetMargin();
+                if (task.Bounds.Top + task.Bounds.Height + task.GetMargin() > maxY) maxY = task.Bounds.Top + task.Bounds.Height+task.GetMargin();
+            }
+            _canvas.Width = maxX;
+            _canvas.Height = maxY;
+        }
+
+        public static Size PrintedSize(string s, FontFamily font, double fontSize)
+        {
+            FormattedText formatted = new FormattedText(s,
+                    CultureInfo.CurrentCulture,
+                    System.Windows.FlowDirection.LeftToRight,
+                    font.GetTypefaces().First(),
+                    fontSize,
+                    Brushes.Black);
+            return new Size(formatted.Width, formatted.Height);
+        }
+
+        public void DrawGanttChart(Canvas _canvas)
+        {
+            const int MARGIN = 10;
+            const int FONT_SIZE = 11;
+            const int LINEHEIGHT = FONT_SIZE + 10;
+            Brush labelColor = Brushes.Black;
+            Brush gridColor = Brushes.Gray;
+
+            _canvas.Children.Clear();
+
+            string label = "";
+            double maxLength = 0;
+            Rect Bounds = new Rect(0,0,0,0);
+            int colStartTime = int.MaxValue;
+            int colEndTime = 0;
+            
+            // Get TextSize and ColumnsCount
+            for (int row = 0; row < Tasks.Count; row++)
+            {
+                label = $"{Tasks[row].Index.ToString()}. {Tasks[row].Name}";
+                if (label.Length * FONT_SIZE * 0.6 > maxLength)
+                {
+                    maxLength = label.Length * FONT_SIZE * 0.6;  
+                }
+                if (Tasks[row].StartTime < colStartTime) colStartTime = Tasks[row].StartTime;
+                if (Tasks[row].EndTime > colEndTime) colEndTime = Tasks[row].EndTime;
+            }
+            // Draw TaskNames
+            for (int row = 0; row < Tasks.Count; row++)
+            {
+                label = $"{Tasks[row].Index.ToString()}. {Tasks[row].Name}";
+                
+                Bounds = new Rect(MARGIN, MARGIN + (row+1) * LINEHEIGHT, maxLength, LINEHEIGHT);
+                _canvas.DrawLabel(Bounds, label, Brushes.Transparent, labelColor, HorizontalAlignment.Left, VerticalAlignment.Center, FONT_SIZE, 0);
+                //_canvas.DrawRectangle(Bounds, Brushes.Transparent, labelColor, 1);
+            }
+            // Draw ColumnHeaders
+            for (int col = colStartTime; col <= colEndTime+1; col++)
+            {
+                Bounds = new Rect(MARGIN+maxLength+(col-colStartTime)*LINEHEIGHT, MARGIN, LINEHEIGHT, LINEHEIGHT);
+                _canvas.DrawLabel(Bounds, col.ToString(), Brushes.Transparent, gridColor, HorizontalAlignment.Center, VerticalAlignment.Center, FONT_SIZE, 0);
+                //_canvas.DrawRectangle(Bounds, Brushes.Transparent, labelColor, 1);
+            }
+            //Draw Grid
+            for (int row = 0; row <= Tasks.Count+1; row++)
+            {
+                _canvas.DrawLine(new Point(MARGIN + maxLength, MARGIN + row * LINEHEIGHT),
+                    new Point(MARGIN + maxLength + LINEHEIGHT * (colEndTime - colStartTime+2), MARGIN + row * LINEHEIGHT), gridColor, 1);
+            }
+            for (int col = colStartTime; col <= colEndTime+2; col++) 
+            {
+                _canvas.DrawLine(new Point(MARGIN + maxLength + (col - colStartTime ) * LINEHEIGHT, MARGIN),
+                    new Point(MARGIN + maxLength + (col - colStartTime ) * LINEHEIGHT, MARGIN + (Tasks.Count+1) * LINEHEIGHT), gridColor, 1);
+            }
+
+
+            /*
+            for (int col = 0; col < Columns.Count; col++)
+            {
+                for (int row = 0; row < Columns[col].Count; row++)
+                {
+                    Task task = Columns[col][row];
+                    task.SetBounds(col, row);
+                }
+            }
+
+            foreach (Task task in Tasks)
+            {
+                task.DrawLinesToPrereqs(_canvas);
+            }
+            double maxX = 0;
+            double maxY = 0;
+            foreach (Task task in Tasks)
+            {
+                task.DrawTaskBox(_canvas);
+                if (task.Bounds.Left + task.Bounds.Width + task.GetMargin() > maxX) maxX = task.Bounds.Left + task.Bounds.Width + task.GetMargin();
+                if (task.Bounds.Top + task.Bounds.Height + task.GetMargin() > maxY) maxY = task.Bounds.Top + task.Bounds.Height + task.GetMargin();
+            }
+            _canvas.Width = maxX;
+            _canvas.Height = maxY;
+            */
+        }
+        public void VerifySort()
+        {
+            if (SortedTasks == null)
+            {
+                return;
+            }
+            int countSorted = 0;
+            int countTasks = 0;
+            for (int i = 0; i < Tasks.Count; i++)
+            {
+                Tasks[i].SortIndex = -1;
+            }
+            for (int i = 0; i < SortedTasks.Count; i++)
+            {
+                SortedTasks[i].SortIndex = i;
+            }
+            foreach (Task task in Tasks)
+            {
+                countTasks++;
+                if (task.PrereqTasks.TrueForAll(t => t.SortIndex < task.SortIndex))
+                {
+                    countSorted++;
+                }
+            }
+            if (countSorted < countTasks)
+            {
+                MessageBox.Show($"Sorted only {countSorted} out of {countTasks} tasks.");
+            }
+            else
+            {
+                MessageBox.Show($"Successfully sorted {countSorted} out of {countTasks} tasks.");
+            }
+            
+        }
+        public Task ReadTask(StreamReader _sr)
+        {
+            while (true)
+            {
+                string line = _sr.ReadLine();
+                if (line == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    line = line.Trim();
+                    if (line.Length > 0)
+                    {
+                        var tokens = new List<String>();
+                        int start = 0;
+                        int count = 0;
+                        int position;
+                        do
+                        {
+                            position = line.IndexOf(',', start);
+                            if (position >= 0)
+                            {
+                                count++;
+                                tokens.Add(line.Substring(start, position - start).Trim());
+                                start = position + 1;
+                            }
+                        } while (position > 0 && count < 3);
+                        if (count == 3)
+                        {
+                            tokens.Add(line.Substring(start).Trim());
+
+                            List<int> pre = new List<int>();
+
+                            string[] prenumbers = tokens[3].Split(',');
+                            foreach (string num in prenumbers)
+                            {
+                                string num2 = num.Replace("[", "").Replace("]", "").Trim();
+                                if (num2 != "")
+                                {
+                                    pre.Add(int.Parse(num2));
+                                }
+                            }
+
+                            Task task = new Task(int.Parse(tokens[0]), tokens[2], int.Parse(tokens[1]), pre);
+
+                            return task;
+                        }
+                                               
+                        break;
+                    }
+                }
+            }
+            return null;
+        }
+        public void LoadPoFile(string _filename)
+        {
+            Tasks = new List<Task>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(_filename))
+                {
+                    Task task;
+                    while ((task = ReadTask(sr)) != null)
+                    {
+                        Tasks.Add(task);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            foreach(Task task in Tasks)
+            {
+                task.NumbersToTasks(Tasks);
+            }
+        }
+    }
+}
---
+++ Scheduling4_GanttCharts/Window1.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+
+namespace gantt_charts
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private PoSorter Sorter = new PoSorter();
+
+        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            unsortedListBox.Items.Clear();
+            sortedListBox.Items.Clear();
+            mainCanvas.Children.Clear();
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.DefaultExt = ".po";
+                dialog.Filter = "Partial Ordering Files|*.po|All Files|*.*";
+
+                // Display the dialog.
+                bool? result = dialog.ShowDialog();
+                if (result == true)
+                {
+                    Sorter.LoadPoFile(dialog.FileName);
+
+                    if (Sorter.Tasks != null)
+                    {
+                        foreach (Task task in Sorter.Tasks)
+                        {
+                            unsortedListBox.Items.Add(task.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            sortButton.IsEnabled = true;
+        }
+
+        private void ExitCommand_Executed(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void sortButton_Click(object sender, RoutedEventArgs e)
+        {
+            Sorter.TopoSort();
+
+            sortedListBox.Items.Clear();
+
+            if (Sorter.SortedTasks != null)
+            {
+                foreach (Task task in Sorter.SortedTasks)
+                {
+                    sortedListBox.Items.Add(task.ToString());
+                }
+            }
+
+            //Sorter.VerifySort();
+
+            Sorter.BuildPertChart();
+            //Sorter.DrawPertChart(mainCanvas);
+            Sorter.DrawGanttChart(mainCanvas);
+        }
+    }
+}
